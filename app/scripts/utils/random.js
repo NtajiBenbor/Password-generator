54f7daa19067a2a31f20ecc6594c9433a3143875
Ntaji Benedict <benedictntaji@gmail.com>
2025-01-20 22:18:50 +0100

    Make changes to the project file structure and remove gulp setup

--- app/scripts/utils/random.js
+++ app/scripts/utils/random.js
@@ -1,89 +1,6 @@
 // Initialize Imports
 import {resetPasswordStrengthIndicator, passwordStrengthIndicator} from "./validators.js";
 
-// GENERATE LETTER FUNC
-function generateLetters(sharedVal) {
-  // letter logic
-  let charCode = 97;
-  const letterArry = [];
-  let randLetterArry = [];
-
-  // generate the 26(A-Z) alphabets using charCode property.
-  while (charCode <= 122) {
-    letterArry.push(String.fromCharCode(charCode));
-    charCode++;
-  }
-  // create a new array by randomly selecting letters from the letterArry
- //   the loop iteration count is determined by the functions argument
-  for (let i = 0; i < sharedVal; i++) {
-    let randNo = Math.floor(Math.random() * letterArry.length-1) + 1;
-    randLetterArry.push(letterArry[randNo]);
-  }
-  // Map over the randLetterArry to create mixture of uppercase and lowercase letters
-  randLetterArry = randLetterArry.map((val) => {
-    if (randLetterArry.indexOf(val) % 2 === 0) {
-      return val.toUpperCase();
-    }
-    return val;
-  });
-  //return the modified/randomised array from the func
-  return randLetterArry;
-}
-
-// GENERATE NUMBERS FUNC
-function generateNumbers(sharedVal) {
-  // number logic
-  const numsArr = [];
-// generate random numbers between 0 and 9;
-// the loop iteration is determined by the value of the function argument 
-  for (let i = 0; i < sharedVal; i++) {
-    let randNo = Math.floor(Math.random() * 8) + 1;
-    numsArr.push(randNo);
-  }
-  return numsArr;
-}
-
-// GENERATE SPECIAL CHARACTERS FUNC
-function generateSpecialChars(sharedVal) {
-	// special Char
-	const specialCharArr = ["$", "%", "&", "@", "?","!"];
-	let randSpecChars = [];
-	 //   the loop iteration count is determined by the functions argument
-	for (let i = 0; i < sharedVal; i++) {
-	  let randNo = Math.floor(Math.random() * specialCharArr.length);
-	  randSpecChars.push(specialCharArr[randNo]);
-	}
-	return randSpecChars;
-}
-
-// SHUFFLE PASSWORD CHAR FUNC
-function ShufflePasswordChars(arr1, arr2, arr3) {
-// concatenate the specialcharArry, numbersArry & letterArray to form a new array
-  let passwordArry = arr1.concat(arr3, arr2);
-// filter out falsey values from the new password array 
-  passwordArry = passwordArry.filter(item =>{
-	if(item != undefined){
-		return item
-	}
-  })
-  console.log(passwordArry);
-  let currentIndex = passwordArry.length;
-// using a while loop and the length of the newly created password array
-  while (currentIndex != 0) {
-	// in each iteration of the loop
-	// create a random index value(variable) using the current index value(variable)
-	// the subtract on from the currentIndex value. this will end the loop when its value is === 0
-    let randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex--;
-	// using destrusturing, swap the value of the password array using the currentIndex and RandomIndex values
-	// starting from its highest index to its lowest index(0) on each iteration.
-	// ensentially shuffle the values of the array in place.
-    [passwordArry[currentIndex], passwordArry[randomIndex]] = 
-	[passwordArry[randomIndex],passwordArry[currentIndex]];
-  }
-  return passwordArry;
-}
-
 // GENERATE PASSWORD FUNC
 function generatePassword(passwordlength) {
   const passwordOutput = document.querySelector(".password-output");
@@ -175,5 +92,91 @@ function generatePassword(passwordlength) {
   copyPaswordBtn.classList.add("show");
   return passwordOutput.textContent;
 }
+
+
+// GENERATE LETTER FUNC
+function generateLetters(sharedVal) {
+  // letter logic
+  let charCode = 97;
+  const letterArry = [];
+  let randLetterArry = [];
+
+  // generate the 26(A-Z) alphabets using charCode property.
+  while (charCode <= 122) {
+    letterArry.push(String.fromCharCode(charCode));
+    charCode++;
+  }
+  // create a new array by randomly selecting letters from the letterArry
+ //   the loop iteration count is determined by the functions argument
+  for (let i = 0; i < sharedVal; i++) {
+    let randNo = Math.floor(Math.random() * letterArry.length-1) + 1;
+    randLetterArry.push(letterArry[randNo]);
+  }
+  // Map over the randLetterArry to create mixture of uppercase and lowercase letters
+  randLetterArry = randLetterArry.map((val) => {
+    if (randLetterArry.indexOf(val) % 2 === 0) {
+      return val.toUpperCase();
+    }
+    return val;
+  });
+  //return the modified/randomised array from the func
+  return randLetterArry;
+}
+
+// GENERATE NUMBERS FUNC
+function generateNumbers(sharedVal) {
+  // number logic
+  const numsArr = [];
+// generate random numbers between 0 and 9;
+// the loop iteration is determined by the value of the function argument 
+  for (let i = 0; i < sharedVal; i++) {
+    let randNo = Math.floor(Math.random() * 8) + 1;
+    numsArr.push(randNo);
+  }
+  return numsArr;
+}
+
+// GENERATE SPECIAL CHARACTERS FUNC
+function generateSpecialChars(sharedVal) {
+	// special Char
+	const specialCharArr = ["$", "%", "&", "@", "?","!"];
+	let randSpecChars = [];
+	 //   the loop iteration count is determined by the functions argument
+	for (let i = 0; i < sharedVal; i++) {
+	  let randNo = Math.floor(Math.random() * specialCharArr.length);
+	  randSpecChars.push(specialCharArr[randNo]);
+	}
+	return randSpecChars;
+}
+
+// SHUFFLE PASSWORD CHAR FUNC
+function ShufflePasswordChars(arr1, arr2, arr3) {
+// concatenate the specialcharArry, numbersArry & letterArray to form a new array
+  let passwordArry = arr1.concat(arr3, arr2);
+// filter out falsey values from the new password array 
+  passwordArry = passwordArry.filter(item =>{
+	if(item != undefined){
+		return item
+	}
+  })
+  console.log(passwordArry);
+  let currentIndex = passwordArry.length;
+// using a while loop and the length of the newly created password array
+  while (currentIndex != 0) {
+	// in each iteration of the loop
+	// create a random index value(variable) using the current index value(variable)
+	// the subtract on from the currentIndex value. this will end the loop when its value is === 0
+    let randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+	// using destrusturing, swap the value of the password array using the currentIndex and RandomIndex values
+	// starting from its highest index to its lowest index(0) on each iteration.
+	// ensentially shuffle the values of the array in place.
+    [passwordArry[currentIndex], passwordArry[randomIndex]] = 
+	[passwordArry[randomIndex],passwordArry[currentIndex]];
+  }
+  return passwordArry;
+}
+
+
 
 export {generatePassword}
